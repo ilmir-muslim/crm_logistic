19fbd45b02cc6465db096b325d1fd1ccc9f1b281
ilmir-muslim <ilmir_gf@mail.ru>
2025-12-14 09:19:11 +0300

    правки

--- crm_logistic/settings.py
+++ crm_logistic/settings.py
@@ -115,13 +115,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),  
-]
+STATICFILES_DIRS = []
 
 if not os.path.exists(os.path.join(BASE_DIR, "static")):
     os.makedirs(os.path.join(BASE_DIR, "static"))
-    
+
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Media files
