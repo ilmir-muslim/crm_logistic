6924ad6498a159804e635b2a8bab18d06aaac0ec
ilmir-muslim <ilmir_gf@mail.ru>
2026-01-26 16:03:53 +0300

    исправлен баг при создании пользователя

--- users/admin.py
+++ users/admin.py
@@ -8,6 +8,15 @@ class UserProfileInline(admin.StackedInline):
     model = UserProfile
     can_delete = False
     verbose_name_plural = "Профиль"
+    extra = 1 
+    max_num = 1  
+
+    def get_formset(self, request, obj=None, **kwargs):
+        if obj is None:
+            self.extra = 0
+        else:
+            self.extra = 1
+        return super().get_formset(request, obj, **kwargs)
 
 
 class CustomUserAdmin(UserAdmin):
--- users/models.py
+++ users/models.py
@@ -35,11 +35,6 @@ class UserProfile(models.Model):
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    """Создает профиль пользователя только при создании пользователя"""
     if created:
-        UserProfile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    if hasattr(instance, "profile"):
-        instance.profile.save()
+        UserProfile.objects.get_or_create(user=instance)
